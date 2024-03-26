bd5b51e92115d525860905a31b834335c2c0a2b9
eugenepurp <jhongil3007@gmail.com>
2024-03-26 12:04:52 +0300

    Финальный проект

--- main.py
+++ main.py
@@ -1,6 +1,44 @@
 from collections import OrderedDict
 
 
+class LZ78Coding:
+
+    def __init__(self, text):
+        self.text = text
+
+    def lz78_compress(self):
+        dictionary = {}
+        encoded_text = []
+        current_phrase = ""
+
+        for symbol in self.text:
+            current_phrase += symbol
+            if current_phrase not in dictionary:
+                if len(current_phrase) == 1:
+                    encoded_text.append((0, symbol))
+                else:
+                    encoded_text.append((dictionary[current_phrase[:-1]], symbol))
+                dictionary[current_phrase] = len(dictionary) + 1
+                current_phrase = ""
+
+        return encoded_text
+
+    def lz78_decompress(self, compressed_text):
+        dictionary = {0: ''}
+        decompressed_text = ""
+
+        for index, symbol in compressed_text:
+            if index == 0:
+                decompressed_text += symbol
+                dictionary[len(dictionary)] = symbol
+            else:
+                phrase = dictionary[index] + symbol
+                decompressed_text += phrase
+                dictionary[len(dictionary)] = phrase
+
+        return decompressed_text
+
+
 class IntervalCoding:
 
     def __init__(self, text):
@@ -53,13 +91,27 @@ class IntervalCoding:
 
 
 def main():
-    text = 'Привет, как твои дела? У меня все просто замечательно, я написал алгоритм интервального кодирования!'
-    my_object = IntervalCoding(text)
-    intervals = my_object.create_intervals()
-    codes = my_object.assign_codes(intervals)
-    encoded_text = my_object.get_encoded_text(codes)
-    decoded_text = my_object.decode(encoded_text, codes)
-    print(decoded_text)
+    with open('text.txt', 'r', encoding='utf-8') as file:
+        text = file.read()
+    my_object_interval = IntervalCoding(text)
+    intervals = my_object_interval.create_intervals()
+    codes = my_object_interval.assign_codes(intervals)
+    encoded_text = my_object_interval.get_encoded_text(codes)
+
+    with open('interval.txt', 'w', encoding='utf-8') as file:
+        file.write(encoded_text)
+
+    my_object_lz78 = LZ78Coding(encoded_text)
+    compressed_text = my_object_lz78.lz78_compress()
+
+    with open('lz78.txt', 'w', encoding='utf-8') as file:
+        file.write(str(compressed_text))
+
+    decompressed_text = my_object_lz78.lz78_decompress(compressed_text)
+    decoded_text = my_object_interval.decode(decompressed_text, codes)
+
+    with open('result.txt', 'w', encoding='utf-8') as file:
+        file.write(decoded_text)
 
 
 if __name__ == '__main__':
